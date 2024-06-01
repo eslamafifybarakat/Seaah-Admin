c10f730e842f3aeb8e8911a900261c2a0ad208bb
eslamfront55 <eslamfront55@gmail.com>
2024-06-01 14:29:42 +0300

    Start FAQs List

---
+++ src/app/components/dashboard/services/faqs.service.ts
@@ -0,0 +1,44 @@
+import { environment } from '../../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { roots } from '../../../shared/configs/roots';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FAQsService {
+  baseUrl: string = environment?.apiUrl;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getFAQsList(page?: number, per_page?: number, search?: string): Observable<any> {
+    let params = new HttpParams();
+    if (page) {
+      params = params?.append("page_number", page);
+    }
+    if (per_page) {
+      params = params?.append("page_size", per_page);
+    }
+    if (search) {
+      params = params?.append("search", search);
+    } else {
+      params = params?.append("search", '');
+    }
+    return this.http?.get(`${this.baseUrl}/${roots?.dashboard?.faqs.getAll}`, { params: params });
+  }
+  addEditBank(data: any, id?: number): Observable<any> {
+    if (id) {
+      return this.http?.post(`${this.baseUrl}/${roots?.dashboard?.organizations.allOrganizations}/${id}`, data)
+    }
+    return this.http?.post(`${this.baseUrl}/${roots?.dashboard?.organizations.allOrganizations}`, data);
+  }
+  getBankById(id: any): Observable<any> {
+    return this.http?.get(`${this.baseUrl}/${roots?.dashboard?.organizations.allOrganizations}/${id}`);
+  }
+  deleteFaqById(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/${roots?.dashboard.organizations.allOrganizations}/` + id);
+  }
+}
